4bca3a56c92dfd278875b8bd526d85d8d79e9ec0
Lokesh04karanam <111414877+Lokesh04karanam@users.noreply.github.com>
2022-11-26 01:51:05 +0530

    Update and rename userreviews.py to reviews.py

---
+++ reviews.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import re
+import pandas as pd
+import json
+import csv
+
+url = "https://www.imdb.com/list/ls000634294/?sort=user_rating,desc&st_dt=&mode=detail&page=3"
+response = requests.get(url, headers={"Accept-Language": "en"})
+soup = BeautifulSoup(response.text, "html.parser")
+movies = soup.find_all("h3.lister-item-header")
+links = soup.find_all('a',attrs={'href':re.compile("/title/*")})
+#Link = [a.attrs.get("href") for a in links]
+URL = []
+for index in links[1:]:
+    URL.append("https://www.imdb.com"+index.attrs.get('href')+"reviews?ref_=tt_ql_sm")
+i = 0
+for element in URL:
+    if i%2 == 0:
+        pass
+    else:
+        URL.remove(element)
+    i = i+1
+Reviews = []
+for i  in URL:
+    Response = requests.get(i, headers={"Accept-Language": "en"})
+    SOUP = BeautifulSoup(Response.content, "html.parser")
+    x = SOUP.find_all('a',attrs={'href':re.compile("/review/rw*"),'class':"title"})
+    l=[]
+    k=0
+    for j in x:
+        if k>2:
+            break
+        l.append(j.text) 
+        k=k+1
+    print(l)
+
+
+            
